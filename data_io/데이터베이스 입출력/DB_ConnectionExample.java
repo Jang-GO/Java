8f98f9a2404a48694ecc9827b6fec3ddd6469da4
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2023-03-22 15:37:22 +0900

    Update DB_ConnectionExample.java

--- data_io/데이터베이스 입출력/DB_ConnectionExample.java
+++ data_io/데이터베이스 입출력/DB_ConnectionExample.java
@@ -12,7 +12,7 @@ public class DB_ConnectionExample {
 			Class.forName("com.mysql.cj.jdbc.Driver"); //Class.forName() : 문자열로 주어진 JDBC 드라이버 클래스를 빌드패스에서 찾고, 메모리로 로딩
 			
 			//연결하기
-			conn = DriverManager.getConnection(
+			conn = DriverManager.getConnection(               // 매개변수 연결문자열, 사용자, 비밀번호
 				"jdbc:mysql://localhost:3306/thisisjava",
 				"java",
 				"mysql"
