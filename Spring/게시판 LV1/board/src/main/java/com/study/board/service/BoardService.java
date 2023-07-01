838aa5f8962f8cfb4ab118121f92dd7893d5b5a4
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2023-07-01 21:39:29 +0900

    Add files via upload

---
+++ Spring/게시판 LV1/board/src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,37 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service // 컨트롤러에서 이용
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    //글 작성 처리
+    public void write(Board board) {
+        boardRepository.save(board);
+    }
+
+    //게시글 리스트 처리
+    public Page<Board> boardList(Pageable pageable){
+        return boardRepository.findAll(pageable);
+    }
+
+    //특정 게시글 불러오기
+    public Board boardView(Integer id){
+        return boardRepository.findById(id).get(); // 파인드 바이 아이디 하면 옵셔널로 받아옴 그래서 get을하면 사라진대
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
+    }
+}
